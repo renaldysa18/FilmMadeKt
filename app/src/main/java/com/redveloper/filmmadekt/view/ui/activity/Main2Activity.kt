0568ce1f96ee7d60583761707e41569140705a42
renaldy <renalraion@gmail.com>
2019-07-27 10:06:57 +0700

    setengah nih

---
+++ app/src/main/java/com/redveloper/filmmadekt/view/ui/activity/Main2Activity.kt
@@ -0,0 +1,75 @@
+package com.redveloper.filmmadekt.view.ui.activity
+
+import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.app.Fragment
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.Menu
+import android.view.MenuItem
+import com.redveloper.filmmadekt.R
+import com.redveloper.filmmadekt.view.ui.fragment.favorite.FavoriteFragment
+import com.redveloper.filmmadekt.view.ui.fragment.movie.MovieFragment
+import com.redveloper.filmmadekt.view.ui.fragment.tvshow.TvshowFragment
+
+class Main2Activity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        navView.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onBackPressed() {
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main2, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        when (item.itemId) {
+            R.id.nav_movie -> moveFragment(MovieFragment())
+            R.id.nav_tvshow -> moveFragment(TvshowFragment())
+            R.id.nav_favorite -> moveFragment(FavoriteFragment())
+        }
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    fun moveFragment(fragment: Fragment) {
+
+    }
+}
