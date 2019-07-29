b7a779b18922434c0ee55e7088b2341971377e94
renaldy <renalraion@gmail.com>
2019-07-29 09:32:07 +0700

    <add> add data popular movie

--- app/src/main/java/com/redveloper/filmmadekt/view/ui/fragment/movie/popular/AdapterPopularMovie.kt
+++ app/src/main/java/com/redveloper/filmmadekt/view/ui/fragment/movie/popular/AdapterPopularMovie.kt
@@ -4,8 +4,10 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
 import com.redveloper.filmmadekt.R
 import com.redveloper.filmmadekt.model.movie.ResponPopularMovie
+import kotlinx.android.synthetic.main.list_item.view.*
 
 class AdapterPopularMovie(val items : List<ResponPopularMovie.Result>?) : RecyclerView.Adapter<AdapterPopularMovie.ViewHolder>(){
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
@@ -21,11 +23,21 @@ class AdapterPopularMovie(val items : List<ResponPopularMovie.Result>?) : Recycl
     }
 
     override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
-
+        p0.binding(items?.get(p1))
     }
 
     class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+        fun binding(get: ResponPopularMovie.Result?) {
+            Glide.with(itemView.context)
+                .load(itemView.context.resources.getString(R.string.BASE_IMAGE) + get?.poster_path)
+                .into(itemView.imageview_listmovie)
 
+            itemView.textview_title_listmovie.setText(get?.title)
+            itemView.textview_description_listmovie.setText(get?.overview)
+            itemView.textview_vote_listmovie.setText(get?.vote_count.toString())
+            itemView.textview_rating_listmovie.setText(get?.vote_average.toString())
+            itemView.textview_popularity_listmovie.setText(get?.popularity.toString())
+        }
     }
 
 }
