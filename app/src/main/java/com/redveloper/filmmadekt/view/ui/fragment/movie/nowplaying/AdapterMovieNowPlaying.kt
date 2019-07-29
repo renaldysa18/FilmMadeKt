fe79c7a5d8e2eab7c5d84ac98a0f30e551cd9513
renaldy <renalraion@gmail.com>
2019-07-29 08:57:53 +0700

    <add> add data now playing

--- app/src/main/java/com/redveloper/filmmadekt/view/ui/fragment/movie/nowplaying/AdapterMovieNowPlaying.kt
+++ app/src/main/java/com/redveloper/filmmadekt/view/ui/fragment/movie/nowplaying/AdapterMovieNowPlaying.kt
@@ -4,8 +4,10 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
 import com.redveloper.filmmadekt.R
 import com.redveloper.filmmadekt.model.movie.ResponNowPlayingMovie
+import kotlinx.android.synthetic.main.list_item.view.*
 
 class AdapterMovieNowPlaying(val items : List<ResponNowPlayingMovie.Result>?) :
     RecyclerView.Adapter<AdapterMovieNowPlaying.ViewHolder>() {
@@ -22,11 +24,25 @@ class AdapterMovieNowPlaying(val items : List<ResponNowPlayingMovie.Result>?) :
     }
 
     override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
-
+        p0.binding(p1,items?.get(p1))
     }
 
     class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+        fun binding(
+            p1: Int,
+            data: ResponNowPlayingMovie.Result?
+        ) {
+            Glide.with(itemView.context)
+                .load(itemView.context.resources.getString(R.string.BASE_IMAGE)+data?.poster_path)
+                .into(itemView.imageview_listmovie)
 
+            itemView.textview_title_listmovie.setText(data?.original_title)
+            itemView.textview_description_listmovie.setText(data?.overview)
+
+            itemView.textview_vote_listmovie.setText(data?.vote_average.toString())
+            itemView.textview_rating_listmovie.setText(data?.vote_average.toString())
+            itemView.textview_popularity_listmovie.setText(data?.popularity.toString())
+        }
     }
 
 }
