adb99da69f3825fd7467f471acae8821f3566389
renaldy <renalraion@gmail.com>
2019-08-06 11:55:36 +0700

    <add> todetail from favorite

--- app/src/main/java/com/redveloper/filmmadekt/view/ui/fragment/favorite/AdapterFavoriteMovie.kt
+++ app/src/main/java/com/redveloper/filmmadekt/view/ui/fragment/favorite/AdapterFavoriteMovie.kt
@@ -1,5 +1,6 @@
 package com.redveloper.filmmadekt.view.ui.fragment.favorite
 
+import android.content.Intent
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -7,6 +8,8 @@ import android.view.ViewGroup
 import com.bumptech.glide.Glide
 import com.redveloper.filmmadekt.R
 import com.redveloper.filmmadekt.model.movie.ResponMovie
+import com.redveloper.filmmadekt.utils.Constant
+import com.redveloper.filmmadekt.view.ui.activity.movie.MovieDetailActivity
 import kotlinx.android.synthetic.main.list_item_favorite.view.*
 
 class AdapterFavoriteMovie(val items : List<ResponMovie.Result>) : RecyclerView.Adapter<AdapterFavoriteMovie.ViewHolder>(){
@@ -22,13 +25,28 @@ class AdapterFavoriteMovie(val items : List<ResponMovie.Result>) : RecyclerView.
         p0.binding(items.get(p1))
     }
 
-    class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+    class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView), View.OnClickListener{
         val baseImage = itemView.context.resources.getString(R.string.BASE_IMAGE)
+        private lateinit var dataGlobal: ResponMovie.Result
 
         fun binding(data: ResponMovie.Result) {
             Glide.with(itemView.context)
                 .load(baseImage + data.poster_path)
                 .into(itemView.imageview_list_favorite)
+
+            this.dataGlobal = data
+
+            itemView.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+            toDetail(dataGlobal)
+        }
+
+        private fun toDetail(data: ResponMovie.Result) {
+            val intent : Intent = Intent(itemView.context, MovieDetailActivity::class.java)
+            intent.putExtra(Constant.dataMovie, data)
+            itemView.context.startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/redveloper/filmmadekt/view/ui/fragment/favorite/AdapterFavoriteTvShow.kt
+++ app/src/main/java/com/redveloper/filmmadekt/view/ui/fragment/favorite/AdapterFavoriteTvShow.kt
@@ -1,5 +1,6 @@
 package com.redveloper.filmmadekt.view.ui.fragment.favorite
 
+import android.content.Intent
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -7,9 +8,12 @@ import android.view.ViewGroup
 import com.bumptech.glide.Glide
 import com.redveloper.filmmadekt.R
 import com.redveloper.filmmadekt.model.tvshow.ResponTvShow
+import com.redveloper.filmmadekt.utils.Constant
+import com.redveloper.filmmadekt.view.ui.activity.tvshow.TvshowDetailActivity
 import kotlinx.android.synthetic.main.list_item_favorite.view.*
 
-class AdapterFavoriteTvShow(val items : List<ResponTvShow.Result>) : RecyclerView.Adapter<AdapterFavoriteTvShow.ViewHolder>(){
+class AdapterFavoriteTvShow(val items: List<ResponTvShow.Result>) :
+    RecyclerView.Adapter<AdapterFavoriteTvShow.ViewHolder>() {
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
         return ViewHolder(LayoutInflater.from(p0.context).inflate(R.layout.list_item_favorite, p0, false))
     }
@@ -22,13 +26,27 @@ class AdapterFavoriteTvShow(val items : List<ResponTvShow.Result>) : RecyclerVie
         p0.binding(items.get(p1))
     }
 
-    class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
         val baseImage = itemView.context.resources.getString(R.string.BASE_IMAGE)
-
+        lateinit var dataGlobal: ResponTvShow.Result
         fun binding(data: ResponTvShow.Result) {
             Glide.with(itemView.context)
                 .load(baseImage + data.poster_path)
                 .into(itemView.imageview_list_favorite)
+
+            this.dataGlobal = data
+
+            itemView.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+            toDetail(dataGlobal)
+        }
+
+        private fun toDetail(data: ResponTvShow.Result) {
+            val intent: Intent = Intent(itemView.context, TvshowDetailActivity::class.java)
+            intent.putExtra(Constant.dataMovie, data)
+            itemView.context.startActivity(intent)
         }
     }
 }
